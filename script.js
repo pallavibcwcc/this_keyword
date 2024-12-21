85be7aae65b46f58b9dee534a6d29577fc8d2610
pallavibcwcc <pallavi.bcwcc@gmail.com>
2024-12-21 11:37:02 +0530

    this

---
+++ script.js
@@ -0,0 +1,8 @@
+const College = {
+    Name:"Bcwcc",
+    location:"Banglore",
+    DisplayInfo:function(){
+        console.log("my collge Name is"+" "+this.Name+ "in"+" "+this.location)
+    }
+}
+College.DisplayInfo();
